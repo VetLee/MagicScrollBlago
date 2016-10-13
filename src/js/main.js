838da543269546e3f227d73787b002bd77ae3006
vet <kuchikvit3008@gmail.com>
2016-10-13 19:43:56 +0300

    + canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,75 +1,52 @@
+//= glfx.js
 
-$(function() {
+window.onload = function() {
+  
+  try {
+    var canvas = fx.canvas();
+  } catch (e) {
+    alert(e);
+    return;
+  }
 
-  var scrollMagicController = new ScrollMagic();
+  var image = document.getElementById('image');
+  var texture = canvas.texture(image);
+  canvas.draw(texture).update();
+  image.parentNode.insertBefore(canvas, image);
+  image.parentNode.removeChild(image);
 
-  // Блюр фона
-  var blurElement = {a:8, b:0.7};
-  var blurTween = TweenMax.to(blurElement, 3, {a:0, b:0.7, onUpdate:applyBlurEffects});
-  var blurScene = new ScrollScene({
-    offset: 30,
-    duration: 300
-  })
-  .setTween(blurTween)
-  .addTo(scrollMagicController);
+
+  var controller = new ScrollMagic.Controller();
+
+
+	new ScrollMagic.Scene({offset: 1})
+		.setClassToggle("#header", "white")
+		.addTo(controller);
+
+
+  var scaleTween = TweenMax.to("#wallpapers", 0.3, {scale: 1, ease: Linear.linear});
+
+  new ScrollMagic.Scene({offset: 0, duration: 1400})
+		.setTween(scaleTween)
+		.addTo(controller);
+
+
+  var blurElement = {a:10};
+
+  var blurTween = TweenMax.to(blurElement, 3, {
+    a:0,
+    onUpdate:applyBlurEffects
+  });
+
+  new ScrollMagic.Scene({offset: 0, duration: 600})
+    .setTween(blurTween)
+    .addTo(controller);
 
   function applyBlurEffects() {
-      TweenMax.set(['.blur'], {
-        webkitFilter:"blur(" + blurElement.a + "px) brightness(" + blurElement.b + ")",
-        filter:"blur(" + blurElement.a + "px) brightness(" + blurElement.b + ")"
-      });
+    TweenMax.set('.blur', {
+      webkitFilter:"blur(" + blurElement.a + "px)",
+      filter:"blur(" + blurElement.a + "px)"
+    });
   };
 
-
-
-  // header
-  var headerTween = TweenMax.to('#header', 0, {
-    'background-color': 'rgba(255, 255, 255, 0.9)',
-    'color': '#1f233a'
-  });
-  var headerScene = new ScrollScene({
-    offset: 1
-  })
-  .setTween(headerTween)
-  .addTo(scrollMagicController);
-
-
-
-  // zoom-out
-  var zoomOutElement = {a:2};
-  var zoomOutTween = TweenMax.to(zoomOutElement, 9, {a:1, onUpdate:applyZoomOutAnim});
-  var zoomOutScene = new ScrollScene({
-    offset: 1,
-    duration: 700
-  })
-  .setTween(zoomOutTween)
-  .addTo(scrollMagicController);
-
-  function applyZoomOutAnim() {
-      TweenMax.set(['#background'], {
-        transform: 'scale3d(' + zoomOutElement.a + ', ' + zoomOutElement.a + ', 1)'
-      });
-  };
-
-
-
-  // block-with-text
-  var blockWithTextElement = {a:35};
-  var blockWithTextTween = TweenMax.to(blockWithTextElement, 3, {a:-30, onUpdate:applyTopPos});
-  var blockWithTextScene = new ScrollScene({
-    offset: 20,
-    duration: 500
-  })
-  .setTween(blockWithTextTween)
-  .addTo(scrollMagicController);
-
-  function applyTopPos() {
-      TweenMax.set(['#block-with-text'], {
-        top: blockWithTextElement.a + "%"
-      });
-  };
-
-
-
-  //zoomOutScene.addIndicators();
-});
+};
